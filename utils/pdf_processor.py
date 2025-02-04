b6a60d230c6198460652584a5838c303f551019d
ben-cipherskin <72762527+ben-cipherskin@users.noreply.github.com>
2025-02-03 20:58:33 -0800

    Updated PyPDF2 package

--- utils/pdf_processor.py
+++ utils/pdf_processor.py
@@ -6,4 +6,4 @@ def extract_text_from_pdf(file_path):
         reader = PyPDF2.PdfReader(file)
         for page in reader.pages:
             text += page.extract_text()
-    return text
+    return text 
